7a9f98c0a69a66e214165111628237a0fa50b6ee
pranav <pranavgupta401@gmail.com>
2022-05-03 23:14:34 +0530

    adding : employee daily wage

---
+++ Calculate Daily Employee Wage.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+/**
+ * Calculate Daily  Employee Wage
+ * 
+ * @author : pranav gupta
+ *
+ */
+class EmployeeWage {
+
+	static int wagePerHr = 20;
+	static int fullTimeHr = 8;
+	public static void main(String args[]) {
+
+	System.out.println("Welcome !");
+
+	Random rand = new Random();
+	int isEmployee = rand.nextInt(2);	
+	System.out.println(isEmployee);
+
+	if(isEmployee == 1)
+	{
+		System.out.println("Employee is Present");
+		System.out.println("Daily Employee Wage = " +(wagePerHr * fullTimeHr));
+		
+	}
+	else
+	{
+		System.out.println("Employee is Absent");
+		System.out.println("Daily Employee Wage = 0 " );
+	}
+
+	}
+}
